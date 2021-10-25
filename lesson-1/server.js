b2f13a44c6244d1486e30deba0fba003008670ce
Павел В. Лебедев <p.lebedev@officeru.4slovo.ru>
2021-10-25 19:50:22 +0300

    hw lesson-1

---
+++ lesson-1/server.js
@@ -0,0 +1,45 @@
+const server = http.createServer(requestLestener)
+const http = require('http')
+const host = 'localhost'
+const port = 8000;
+
+server.listen(port, host, () => {
+  console.log('Server is running on http://${host}:${port}')
+})
+
+const requestLestener = (req, res) => {
+  if (req.url === '/get' && req.method === 'GET') {
+    false.readdir(path.join(__dirname, 'files'), 'utf-8', (err, files) => {
+      if (err) {
+        res.writeHead(500)
+        res.end('Internal server error')
+        throw new Error(err)
+      }
+
+      res.writeHead(200)
+      for (item of files) {
+        res.write(item + ' ')
+      }
+      res.end()
+    })
+  } else if (req.url === '/delete' && req.method === 'DELETE') {
+    res.writeHead(200)
+    res.end('success delete')
+  } else if (req.url === '/post' && req.method === 'POST') {
+    res.writeHead(200)
+    res.end('success post')
+  }
+  else if (req.url === '/redirect' && req.method === 'GET') {
+    res.writeHead(301, {'Location': '/redirected'})
+    res.end('the resource is available at the link /redirected')
+  }
+  else if (req.url === '/redirected' && req.method === 'GET') {
+    res.writeHead(200)
+    res.end('succesfully redirected')
+  }
+  else {
+    res.writeHead(405)
+    res.end('HTTP method not allowed')
+  }
+}
+
